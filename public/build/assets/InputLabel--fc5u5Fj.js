0a45b083b480a948e652439dac6fe9302336d0fc
TumiraNgTres <yabaigaming@gmail.com>
2025-10-05 10:20:48 +0800

    minor update

--- public/build/assets/InputLabel--fc5u5Fj.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-BQYH6xId.js";function r({value:e,className:l="",children:a,...n}){return s.jsx("label",{...n,className:"label fieldset"+l,children:s.jsx("span",{className:"fieldset-legend",children:e||a})})}export{r as I};
